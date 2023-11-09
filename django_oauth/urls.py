53c289e9c13487d57231a54e3f52054d72e65330
BlackTea <blacktea3250106@gmail.com>
2023-11-09 19:48:16 +0800

    add dj-rest-auth logout

--- django_oauth/urls.py
+++ django_oauth/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from authentication.views import GoogleLogin
-from django.contrib.auth.views import LogoutView
+from allauth.account.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
